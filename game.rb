06e89054c65b25f3ca8f53d162ff60de437e3f6d
Roberto Arruda <robertoadearruda@gmail.com>
2015-07-25 17:07:27 -0300

    Melhorando o codigo

--- game.rb
+++ game.rb
@@ -1,58 +1,73 @@
 class Game
 
-  def initialize()
-    @attemps = 0
-    setPcNumber()
+  def initialize
+    clear
   end
 
-  def setPcNumber()
+  def clear
+    @attemps = 0
+    setPcNumber
+  end
+
+  def setPcNumber
     @pcNumber = Random.rand(99) + 1
   end
 
-  def play()
+  def play
     @attemps += 1
+
     if @attemps == 1
-      puts ""
-      puts "PC: Adivinhe o numero que estou pensando entre 1 e 100:"
+      puts "\nPC: Adivinhe o numero que estou pensando entre 1 e 100:"
     end
 
-    getPlayerNumber()
-    verify()
+    getPlayerNumber
+    verify
   end
 
-  def getPlayerNumber()
+  def getPlayerNumber
     @playerNumber = gets.to_i
     puts ""
   end
 
-  def verify()
+  def verify
     if @playerNumber == @pcNumber
       puts "PC: Voce acertou #{@pcNumber} (tentativas #{@attemps})!!!"
-      restart()
+      restart
     end
 
+    error
+  end
+
+  def restart
+    puts "PC: Deseja jogar novamente? (s/n)"
+
+    response = gets.chomp
+
+    if !['n', 's'].include? response
+      restart
+    end
+
+    if response == "n"
+      puts "\nPC: Obrigado por jogar!\n\n"
+      exit
+    end
+
+    clear
+    play
+  end
+
+  def error
     error = "menor"
+
     if @playerNumber > @pcNumber
       error = "maior"
     end 
 
     puts "PC: #{@playerNumber} e' #{error} que o numero que eu pensei, tente novamente!"
-    play()
-  end
-
-  def restart()
-    puts "PC: Deseja jogar novamente? (s/n)"
-    if (gets.chomp == "s")
-      setPcNumber()
-      @attemps = 0
-      play()
-    end
-
-    puts "PC: Obrigado por jogar!"
-    exit
+    play
   end
 
 end
 
 game = Game.new
-game.play()
+game.play
